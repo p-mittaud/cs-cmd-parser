c4702374bc96a9ca0417e7c82255bcd22193d93b
p-mittaud <p.mittaud@gmail.com>
2025-05-05 18:57:52 +0200

    [feat] add project and first version which cuts a string in args
    + handling of quote character to handle

---
+++ main/cmd-parser/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace cmd_parser
+{
+    public class CmdLine
+    {
+        private readonly string _command;
+        public string[] args { get; set; } = new string[0];
+
+        private void AddArgument(ref string? inString)
+        {
+            args = args.Append(inString).ToArray()!;
+            inString = null;
+        }
+
+        public CmdLine(string command)
+        {
+            _command = command;
+
+            // Cut line in args
+            string? currentString = null;
+
+            for (int i = 0; i < _command.Length; i++)
+            {
+                if (_command[i] != ' ')
+                {
+                    currentString = currentString == null ? _command[i].ToString() : currentString + _command[i];
+                }
+                else if (currentString != null)
+                {
+                    AddArgument(ref currentString);
+                }
+                if (currentString != null && i + 1 == _command.Length)
+                {
+                    AddArgument(ref currentString);
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var inputLine = Console.ReadLine();
+            if (inputLine != null)
+            {
+                Console.WriteLine($"line is \"{inputLine}\"");
+            }
+            else
+            {
+                return;
+            }
+
+            CmdLine cmdLine = new CmdLine(inputLine);
+
+            Console.WriteLine($"Found {cmdLine.args.Length} arguments: ");
+            foreach (var arg in cmdLine.args)
+            {
+                Console.WriteLine($"\"{arg}\"");
+            }
+        }
+    }
+}
