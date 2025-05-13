50192dff1c5a37bd140cbbb7747f3e1748ac44fe
p-mittaud <p.mittaud@gmail.com>
2025-05-13 07:49:40 +0200

    [feat] cmdLine : removed cmdOption prefix check and clean code
    + Updated variables to readonly
    + Added todo where exceptions should be set up

--- cmd-parser/CmdLine.cs
+++ cmd-parser/CmdLine.cs
@@ -14,23 +14,24 @@
 
     public class CmdLine
     {
-        public string[] args { get; set; } = new string[0];
+        public readonly string[] args = new string[0];
 
-        public string? cmd { get; set; }
+        public readonly string? cmd;
 
-        public string[] baseArgs { get; set; } = new string[0];
+        public readonly string[] baseArgs = new string[0];
 
-        public Dictionary<string, string[]> cmdOptions { get; set; } = new Dictionary<string, string[]>();
+        public readonly Dictionary<string, string[]> cmdOptions = new Dictionary<string, string[]>();
 
-        public readonly CmdConfig[] _cmdConfig;
+        public readonly CmdConfig[] cmdConfigs;
 
-        private void AddArg(ref string? inString)
+        private string[] AddArg(ref string? inString)
         {
-            args = args.Append(inString).ToArray()!;
+            string[] resString = args.Append(inString).ToArray()!;
             inString = null;
+            return resString;
         }
 
-        public CmdLine(string command, CmdConfig[]? inCmdConfig = null, bool bContainsCmd = true)
+        public CmdLine(string command, CmdConfig[]? cmdConfigArray = null, bool bContainsCmd = true)
         {
             // Cut line in args
             string? currentString = null;
@@ -42,7 +43,7 @@
                 {
                     if (bInQuotes)
                     {
-                        AddArg(ref currentString);
+                        args = AddArg(ref currentString);
                     }
                     bInQuotes = !bInQuotes;
                     continue;
@@ -53,17 +54,17 @@
                 }
                 else if (currentString != null)
                 {
-                    AddArg(ref currentString);
+                    args = AddArg(ref currentString);
                 }
                 if (currentString != null && i + 1 == command.Length)
                 {
-                    AddArg(ref currentString);
+                    args = AddArg(ref currentString);
                 }
             }
 
             if (bInQuotes)
             {
-                // throw exception, invalid commandLine
+                // TODO: throw exception, invalid commandLine
                 Console.Error.WriteLine("Invalid Command received in CommandLine!");
             }
 
@@ -72,12 +73,12 @@
                 cmd = args[0];
             }
 
-            _cmdConfig = ValidateCmdConfigArray(inCmdConfig) ?? new CmdConfig[0];
+            cmdConfigs = ValidateCmdConfigArray(cmdConfigArray) ?? new CmdConfig[0];
 
             CreateOptionsCmd();
         }
 
-        public CmdLine(string[] inArgs, CmdConfig[]? cmdConfig = null, bool bContainsCmd = true)
+        public CmdLine(string[] inArgs, CmdConfig[]? cmdConfigArray = null, bool bContainsCmd = true)
         {
             args = inArgs;
 
@@ -86,7 +87,7 @@
                 cmd = args[0];
             }
 
-            _cmdConfig = ValidateCmdConfigArray(cmdConfig) ?? new CmdConfig[0];
+            cmdConfigs = ValidateCmdConfigArray(cmdConfigArray) ?? new CmdConfig[0];
 
             CreateOptionsCmd();
         }
@@ -98,24 +99,31 @@
                 return null;
             }
 
+            // Check if all cmdConfig in cmdConfigArray don't share the same name
+            // If there is: throw exception
+
             foreach (var cmdConfig in cmdConfigArray)
             {
                 if (String.IsNullOrEmpty(cmdConfig.name) || cmdConfig.name.Contains(" "))
                 {
-                    Console.Error.WriteLine("Received invalid cmdCondif: Invalid name!");
+                    // TODO: throw exception: invalid cmdConfig
+                    Console.Error.WriteLine("Received invalid cmdConfig: Invalid name!");
                     return null;
                 }
                 if (cmdConfig.hasArgs && (cmdConfig.argCount == null || cmdConfig.argCount <= 0))
                 {
-                    Console.Error.WriteLine($"Received invalid cmdCondif: Invalid argCount for cmd {cmdConfig.name}!");
+                    // TODO: throw exception: invalid cmdConfig
+                    Console.Error.WriteLine($"Received invalid cmdConfig: Invalid argCount for cmd {cmdConfig.name}!");
                     return null;
                 }
             }
             return cmdConfigArray;
         }
 
-        private void CreateOptionsCmd()
+        private string[] CreateOptionsCmd()
         {
+            string[] foundBaseArgs = new string[0];
+
             int expectedArgCount = 0;
             string? currentOption = null;
             foreach (var arg in args.Skip(cmd != null ? 1 : 0))
@@ -127,39 +135,33 @@
                     {
                         currentOption = null;
                     }
+                    continue;
                 }
-                else if (arg.StartsWith("--") && !arg.Contains(" "))
+                else if (!arg.Contains(" "))
                 {
-                    var cmdOption = Array.Find(_cmdConfig, elem => elem.name == arg);
+                    var cmdOption = Array.Find(cmdConfigs, elem => elem.name == arg);
                     if (cmdOption != null)
                     {
                         if (cmdOptions.ContainsKey(cmdOption.name))
                         {
+                            // TODO: throw exception if cmdOption already contains the key
                             Console.WriteLine($"Warning: command {cmdOption.name} has already been added to args!");
-                            continue;
                         }
-
-                        cmdOptions.Add(cmdOption.name, new string[0]);
-                        if (cmdOption.hasArgs && cmdOption.argCount != null)
+                        else
                         {
-                            currentOption = cmdOption.name;
-                            expectedArgCount = cmdOption.argCount.Value;
+                            cmdOptions.Add(cmdOption.name, new string[0]);
+                            if (cmdOption.hasArgs)
+                            {
+                                currentOption = cmdOption.name;
+                                expectedArgCount = cmdOption.argCount!.Value;
+                            }
                         }
-                        else if (cmdOption.hasArgs)
-                        {
-                            Console.WriteLine($"CmdOption {cmdOption.name} is set to hasArgs = true but argCount is null");
-                        }
-                    }
-                    else
-                    {
-                        baseArgs = baseArgs.Append(arg).ToArray();
+                        continue;
                     }
                 }
-                else
-                {
-                    baseArgs = baseArgs.Append(arg).ToArray();
-                }
+                foundBaseArgs = foundBaseArgs.Append(arg).ToArray();
             }
+            return foundBaseArgs;
         }
 
         public bool ContainsOption(string inOption)
@@ -167,6 +169,12 @@
             return cmdOptions.ContainsKey(inOption);
         }
 
+        /// <summary>
+        /// Returns values associated to a command option.
+        /// </summary>
+        /// <typeparam name="T">The type to convert found options.</typeparam>
+        /// <param name="inOption">Option name to find.</param>
+        /// <returns>Found options or null if empty.</returns>
         public T[]? GetOptionValues<T>(string inOption)
         {
             if (!cmdOptions.ContainsKey(inOption))
@@ -193,6 +201,7 @@
                 }
                 catch (System.FormatException)
                 {
+                    // TODO: Create or throw Exception: Invalid format type
                     Console.WriteLine($"Failed to convert {arg} to type {typeof(T).FullName}");
                     return null;
                 }
